141b3ae17608c2c67c6a6a79f8511b1416b90fb9
yixianzi <272541876@qq.com>
2022-02-09 23:50:18 +0800

    feat: angry

---
+++ src/views/test1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <h1 style="color:red">test1</h1>
+      <button @click=goback>点我返回</button>
+      <div class="t1Wrapper"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    methods:{
+    goback:function(){
+        this.$router.push('/')
+
+    }
+}
+
+}
+
+
+</script>
+
+<style>
+    .t1Wrapper{
+        width: 100px;
+        height: 100px;
+        background-image: linear-gradient(to left top, #da45a8, transparent);
+        background-size: 20px 20px;
+    }
+</style>
